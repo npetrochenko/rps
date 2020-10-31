18f89996761726f98bd473bca82e6640e18d48c5
Nikita Petrochenko <npetrochenko@yahoo.com>
2020-11-01 02:51:19 +0300

    Made a graphic interface

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Rock, Papers, Scissors game as part of Web Development course on The Odin Project
+
+TODO: Counters for both the player and computer, and also need to limit amount of games; 
--- script.js
+++ script.js
@@ -1,63 +1,95 @@
-let playerSelection;
-let computerSelection;
-let a = 0;
-let b = 0;
+let playerSelection, computerSelection;
+let counter = 0;
 
-playerSelection = prompt("Make your choice");
-playerSelection = playerSelection.toUpperCase();
-console.log(playerSelection);
-
-computerSelection = computerPlay();
-console.log(computerSelection);
-
-function computerPlay() {
-    a = Math.random()
-    if (a <= 0.33) {
+function randomComputerChoice() {
+    random = Math.random()
+    if (random <= 0.33) {
         return "ROCK";
-    } else if (a > 0.33 && a < 0.66) {
+    } else if (random <= 0.66) {
         return "PAPER";
-    } else {
+    } else { 
         return "SCISSORS";
-    }
-}
+    };
+};
 
-function playRound(playerSelection, computerSelection) {
-    switch(playerSelection) {
-        
-        case "ROCK":
-            if (computerSelection == "SCISSORS") {
-                alert("You won")
-            } else if (computerSelection == "PAPER") {
-                alert("You lose")
-            } else {
-                alert("Draw")
-            }
+const rockBtn = document.querySelector('.rock');
+const paperBtn = document.querySelector('.paper');
+const scissorsBtn = document.querySelector('.scissors');
+const playerChoice = document.querySelector('.player-choice > i');
+const computerChoice = document.querySelector('.computer-choice > i')
+
+scissorsBtn.addEventListener('click', function() {
+    playerSelection = 'SCISSORS';
+    playerChoice.setAttribute('class', 'fas fa-hand-scissors');
+    computerSelection = randomComputerChoice();
+     if (computerSelection == "ROCK") {
+         computerChoice.setAttribute('class', 'fas fa-hand-rock');
+     } else if (computerSelection == "PAPER") {
+         computerChoice.setAttribute('class', 'fas fa-hand-paper');
+     } else {
+         computerChoice.setAttribute('class', 'fas fa-hand-scissors');
+     };
+     playRound(playerSelection, computerSelection);
+     console.log(counter);
+});
+rockBtn.addEventListener('click', function() {
+    playerSelection = 'ROCK';
+    playerChoice.setAttribute('class', 'fas fa-hand-rock');
+    computerSelection = randomComputerChoice();
+     if (computerSelection == "ROCK") {
+         computerChoice.setAttribute('class', 'fas fa-hand-rock');
+     } else if (computerSelection == "PAPER") {
+         computerChoice.setAttribute('class', 'fas fa-hand-paper');
+     } else {
+         computerChoice.setAttribute('class', 'fas fa-hand-scissors');
+     };
+     playRound(playerSelection, computerSelection);
+     console.log(counter);
+});
+paperBtn.addEventListener('click', function() {
+    playerSelection = 'PAPER';
+    playerChoice.setAttribute('class', 'fas fa-hand-paper');
+    computerSelection = randomComputerChoice();
+     if (computerSelection == "ROCK") {
+         computerChoice.setAttribute('class', 'fas fa-hand-rock');
+     } else if (computerSelection == "PAPER") {
+         computerChoice.setAttribute('class', 'fas fa-hand-paper');
+     } else {
+         computerChoice.setAttribute('class', 'fas fa-hand-scissors');
+     };
+     playRound(playerSelection, computerSelection);
+     console.log(counter);
+});
+
+const menuCounter = document.querySelector('.counter');
+menuCounter.textContent = 0;
+window.addEventListener('click', function() {menuCounter.textContent = counter});
+
+function playRound(p, c) {
+    switch(p) {
+        case 'ROCK':
+            console.log('p is rock');
+            if (c == 'PAPER') {counter--; console.log('c is paper');}
+            else if (c == 'SCISSORS') {counter++; console.log('c is scissors');}
+            else {console.log('c is rock')}
             break;
-
-        case "PAPER":
-            if (computerSelection == "ROCK") {
-                alert("You won")
-            } else if (computerSelection == "SCISSORS") {
-                alert("You lose")
-            } else {
-                alert("Draw")
-            }
+        case 'PAPER':
+            console.log('p is paper');
+            if (c == 'SCISSORS') {counter--; console.log('c is scissors');}
+            else if (c == 'ROCK') {counter++; console.log('c is rock');}
+            else {console.log('c is paper')}
             break;
-
-        case "SCISSORS": 
-            if (computerSelection == "PAPER") {
-                alert("You won")
-            } else if (computerSelection == "ROCK") {
-                alert("You lose")
-            } else {
-                alert("Draw")
-            }
+        case 'SCISSORS': 
+            console.log('p is scissors');
+            if (c == 'ROCK') {counter--; console.log('c is rock');}
+            else if (c == 'PAPER') {counter++; console.log('c is paper');}
+            else {console.log('c is scissors')}
             break;
-        
-        default:
-            alert("Make a right choice")
-    }
-}    
+    };
+};
 
 
-playRound(playerSelection, computerSelection);
+
+
+
+
